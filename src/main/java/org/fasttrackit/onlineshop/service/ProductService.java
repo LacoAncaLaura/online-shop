8ed277f13bb5850bf302478d287837358914fc95
LacoAncaLaura <lala14444444@yahoo.com>
2020-06-15 21:34:14 +0300

    added product repository and service

---
+++ src/main/java/org/fasttrackit/onlineshop/service/ProductService.java
@@ -0,0 +1,32 @@
+package org.fasttrackit.onlineshop.service;
+
+import org.fasttrackit.onlineshop.domain.Product;
+import org.fasttrackit.onlineshop.persistance.ProductRepository;
+import org.fasttrackit.onlineshop.transfer.SaveProductRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+//Spring Beans(Service,Repositories)
+@Service
+public class ProductService {
+//    inversion of control (IoC)
+    private final ProductRepository productRepository;
+
+//    Dependency Injection
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+    public Product createProduct(SaveProductRequest request){
+        //    todo: replace sout with logger
+        System.out.println("Creating product: " + request);
+        Product product = new Product();
+        product.setName(request.getName());
+        product.setDescription(request.getDescription());
+        product.setPrice(request.getPrice());
+        product.setQuantity(request.getQuantity());
+        product.setImageUrl(request.getImageUrl());
+         return   productRepository.save(product);
+
+    }
+}
