22742f62ee2b762981a320cb3479a72a8b53d06e
LacoAncaLaura <lala14444444@yahoo.com>
2020-06-22 21:08:32 +0300

    added getProduct by id method

--- src/main/java/org/fasttrackit/onlineshop/service/ProductService.java
+++ src/main/java/org/fasttrackit/onlineshop/service/ProductService.java
@@ -1,12 +1,15 @@
 package org.fasttrackit.onlineshop.service;
 
 import org.fasttrackit.onlineshop.domain.Product;
+import org.fasttrackit.onlineshop.exception.ResourceNotFoundException;
 import org.fasttrackit.onlineshop.persistance.ProductRepository;
 import org.fasttrackit.onlineshop.transfer.SaveProductRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 //Spring Beans(Service,Repositories)
 @Service
@@ -33,4 +36,21 @@ public class ProductService {
          return   productRepository.save(product);
 
     }
+    public Product getProduct(long id) {
+        LOGGER.info("Retrieving product{}", id);
+
+        Optional<Product> productOptional = productRepository.findById(id);
+        if(productOptional.isPresent()){
+            return productOptional.get();
+        }else{
+            throw new ResourceNotFoundException("Product" + id + "not found");
+        }
+//        or
+//        --Lambda expression--
+//        return productRepository.findById(id)
+//                .orElseThrow(() -> new ResourceNotFoundException("Product" + id + "not found"));
+
+//        Does the same thing like the OPTIONAL above
+    }
+
 }
