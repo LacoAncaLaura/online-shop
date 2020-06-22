1651fdfa9dde6cace7f67a0da3a06401651989f8
LacoAncaLaura <lala14444444@yahoo.com>
2020-06-22 18:57:30 +0300

    added logger for product service

--- src/main/java/org/fasttrackit/onlineshop/service/ProductService.java
+++ src/main/java/org/fasttrackit/onlineshop/service/ProductService.java
@@ -3,12 +3,16 @@ package org.fasttrackit.onlineshop.service;
 import org.fasttrackit.onlineshop.domain.Product;
 import org.fasttrackit.onlineshop.persistance.ProductRepository;
 import org.fasttrackit.onlineshop.transfer.SaveProductRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 //Spring Beans(Service,Repositories)
 @Service
 public class ProductService {
+//Lombok(librarie pt generari automate -> pt getteri si setteri,constructori)
+    private final static Logger LOGGER = LoggerFactory.getLogger(ProductService.class);
 //    inversion of control (IoC)
     private final ProductRepository productRepository;
 
@@ -18,8 +22,8 @@ public class ProductService {
         this.productRepository = productRepository;
     }
     public Product createProduct(SaveProductRequest request){
-        //    todo: replace sout with logger
-        System.out.println("Creating product: " + request);
+        //    todo: replace sout with logger for printing-->replaced with logger.info!!!!For detailed/warning/errors Logger use debug/warn/error instead info
+        LOGGER.info("Creating product{}",request);
         Product product = new Product();
         product.setName(request.getName());
         product.setDescription(request.getDescription());
