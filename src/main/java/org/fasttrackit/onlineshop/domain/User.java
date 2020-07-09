c6cf462a935f005ed3bfc9683f9b1ffba73ba646
LacoAncaLaura <lala14444444@yahoo.com>
2020-07-09 20:27:42 +0300

    .

--- src/main/java/org/fasttrackit/onlineshop/domain/User.java
+++ src/main/java/org/fasttrackit/onlineshop/domain/User.java
@@ -1,9 +1,10 @@
 package org.fasttrackit.onlineshop.domain;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
-
+@Entity
 public class User {
     @Id
     @GeneratedValue
