86897629fdcc07d1ef6024ea2f92a5f817f57cd5
LacoAncaLaura <lala14444444@yahoo.com>
2020-07-09 20:51:02 +0300

    .

--- src/main/java/org/fasttrackit/onlineshop/service/UserService.java
+++ src/main/java/org/fasttrackit/onlineshop/service/UserService.java
@@ -8,7 +8,9 @@ import org.fasttrackit.onlineshop.transfer.CreateUserRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserService {
     private static final Logger LOGGER = LoggerFactory.getLogger(UserService.class);
     private final UserRepository userRepository;
